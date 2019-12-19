c9e5f1dfacd0d09c894c6775f0cce232ec7fa8c7
valentina <valentina@click.msk.ru>
2019-12-19 14:04:56 +0300

    block 4 and fix blocks

--- src/Slogan/Slogan.js
+++ src/Slogan/Slogan.js
@@ -5,6 +5,12 @@ import styles from './slogan.module.css'
 function Slogan() {
     return (
         <div className={styles.slogan}>
+            <div className={styles.slogan_block}>
+                <div className={styles.my_slogan}>Рассматриваю варианты удаленной работы</div>
+                <div className={styles.button_block}>
+                    <button>Нанять меня</button>
+                </div>
+            </div>
 
         </div>
     );
