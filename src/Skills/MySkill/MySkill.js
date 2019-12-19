b66dadba134c11935947a09fdf5263890e28ec7e
valentina <valentina@click.msk.ru>
2019-12-19 12:22:10 +0300

    block 2

---
+++ src/Skills/MySkill/MySkill.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import styles from './my_skill.module.css'
+
+
+function MySkill() {
+    return (
+        <div className={styles.one_skill}>
+            <div className={styles.block_skill}>
+                <div className={styles.icon}>
+
+                </div>
+                <div>React</div>
+            </div>
+            <div className={styles.description_skill}>
+                Подробное описание навыка
+            </div>
+        </div>
+    );
+}
+
+export default MySkill;
--- src/Skills/Skills.js
+++ src/Skills/Skills.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import styles from './skills.module.css'
+import MySkill from "./MySkill/MySkill";
 
 
 function Skills() {
     return (
         <div className={styles.skills}>
-
+            <div className={styles.my_skills_block}>
+                <span>Мои скиллы</span>
+                <div className={styles.my_skills}>
+                    <MySkill />
+                    <MySkill />
+                    <MySkill />
+                </div>
+            </div>
         </div>
     );
 }
